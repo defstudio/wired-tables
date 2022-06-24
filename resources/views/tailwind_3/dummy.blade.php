4f1083c9b53bb4af2eb19cb604f5cfea39681b41
Fabio Ivona <fabio.ivona@defstudio.it>
2022-06-24 18:12:10 +0200

    add dark mode for header
    Took 5 minutes

--- resources/views/tailwind_3/dummy.blade.php
+++ resources/views/tailwind_3/dummy.blade.php
@@ -1,9 +1,30 @@
 <div class="
+    relative
+
+
+    px-6
+    ml-4 h-4 w-4
+    py-3 pt-3
+
+    uppercase
     font-normal
     font-medium
     text-left
     text-xs text-sm text-base text-lg
-    text-gray-500 text-gray-50
+
+    text-gray-500 text-gray-400 text-gray-50
+    group-hover:text-gray-200
+
+    tracking-wider
+
+    whitespace-nowrap
+
+    stroke-gray-400 stroke-gray-200
+    fill-gray-400 fill-gray-200
+    group-hover:stroke-gray-600 group-hover:stroke-gray-400 group-hover:stroke-gray-200
+    group-hover:fill-gray-600 group-hover:fill-gray-400group-hover:fill-gray-200
+
+
 
 
     odd:bg-white even:bg-gray-50
--- resources/views/tailwind_3_prefixed/dummy.blade.php
+++ resources/views/tailwind_3_prefixed/dummy.blade.php
@@ -1,10 +1,31 @@
 <div class="
+    tw-relative
 
+
+    tw-px-6
+    tw-ml-4 tw-h-4 tw-w-4
+    tw-py-3 tw-pt-3
+
+    tw-uppercase
     tw-font-normal
     tw-font-medium
     tw-text-left
     tw-text-xs tw-text-sm tw-text-base tw-text-lg
-    tw-text-gray-500 tw-text-gray-50
+
+    tw-text-gray-500 tw-text-gray-400 tw-text-gray-50
+    group-hover:tw-text-gray-200
+
+    tw-tracking-wider
+
+    tw-whitespace-nowrap
+
+    tw-stroke-gray-400 tw-stroke-gray-200
+    tw-fill-gray-400 tw-fill-gray-200
+    group-hover:tw-stroke-gray-600 group-hover:tw-stroke-gray-400 group-hover:tw-stroke-gray-200
+    group-hover:tw-fill-gray-600 group-hover:tw-fill-gray-400 group-hover:tw-fill-gray-200
+
+
+
 
     odd:tw-bg-white even:tw-bg-gray-50
 "></div>
