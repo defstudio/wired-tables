37b7747de63a83d677d7087b317ee92673bc1385
fabio-ivona <fabio-ivona@users.noreply.github.com>
2023-09-06 08:26:12 +0000

    Lint

--- src/Elements/Column.php
+++ src/Elements/Column.php
@@ -166,7 +166,8 @@ class Column extends Configuration implements Arrayable
         return $this->set(Config::type, ColumnType::boolean);
     }
 
-    public function clamp(int $rows = 1): static{
+    public function clamp(int $rows = 1): static
+    {
         return $this->set(Config::clamp, $rows);
     }
 
@@ -376,6 +377,4 @@ class Column extends Configuration implements Arrayable
     {
         //TODO
     }
-
-
 }
