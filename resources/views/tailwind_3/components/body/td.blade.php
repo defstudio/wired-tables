61f5890e2a907ac2dd6d7bace01be1e0ae9f0d6e
Fabio Ivona <fabio.ivona@defstudio.it>
2023-11-07 10:48:43 +0100

    fix compact table

--- resources/views/tailwind_3/components/body/td.blade.php
+++ resources/views/tailwind_3/components/body/td.blade.php
@@ -15,9 +15,11 @@ $content = $column->render();
 $attributes = $attributes->merge([
     'wire:key' => "wt-$this->id-row-{$this->getRowId($model)}-cell"
 ])->class([
+    "px-4 py-2" => $this->config(\DefStudio\WiredTables\Enums\Config::compact_table),
+    "px-6 py-3" => !$this->config(\DefStudio\WiredTables\Enums\Config::compact_table),
     "px-6 py-3",
     "font-medium",
-    "whitespace-nowrap" => !$column->get(\DefStudio\WiredTables\Enums\Config::wrapText),
+    "whitespace-nowrap" => !$column->get(\DefStudio\WiredTables\Enums\Config::wrapText) && !$this->config(\DefStudio\WiredTables\Enums\Config::compact_table),
     "min-w-[15rem]" => $column->get(\DefStudio\WiredTables\Enums\Config::wrapText) && \Illuminate\Support\Str::of($content->toHtml())->trim()->isNotEmpty(),
     $column->getTextClasses(),
 ]);
