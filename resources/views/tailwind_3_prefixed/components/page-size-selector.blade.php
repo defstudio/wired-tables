820e94ee3dfcf1735e71c4567b6239d565a27028
Fabio Ivona <fabio.ivona@defstudio.it>
2023-11-10 10:31:08 +0100

    fix page size selector

--- resources/views/tailwind_3_prefixed/components/page-size-selector.blade.php
+++ resources/views/tailwind_3_prefixed/components/page-size-selector.blade.php
@@ -3,12 +3,16 @@
 use DefStudio\WiredTables\WiredTable;
 
 /** @var WiredTable $this */
+
+$availablePageSize = $this->config(\DefStudio\WiredTables\Enums\Config::available_page_sizes);
+
 ?>
 
-
-<select {{$attributes->class("tw-bg-transparent tw-border focus-visible:tw-outline-0 tw-border-solid tw-px-2 tw-pr-7 tw-py-2 tw-border-gray-300 focus:tw-border-indigo-300 focus:tw-ring focus:tw-ring-indigo-200 focus:tw-ring-opacity-50 tw-rounded-md tw-shadow-sm tw-text-sm tw-text-gray-700 tw-cursor-pointer")}} wire:model="pageSize">
-    @foreach($this->config(\DefStudio\WiredTables\Enums\Config::available_page_sizes) as $availPagSize)
-        <option value="{{$availPagSize}}">{{ucfirst($availPagSize)}}</option>
-    @endforeach
-</select>
+@if($availablePageSize && count($availablePageSize)>1)
+    <select {{$attributes->class("tw-bg-transparent tw-border focus-visible:tw-outline-0 tw-border-solid tw-px-2 tw-pr-7 tw-py-2 tw-border-gray-300 focus:tw-border-indigo-300 focus:tw-ring focus:tw-ring-indigo-200 focus:tw-ring-opacity-50 tw-rounded-md tw-shadow-sm tw-text-sm tw-text-gray-700 tw-cursor-pointer")}} wire:model="pageSize">
+        @foreach($this->config(\DefStudio\WiredTables\Enums\Config::available_page_sizes) as $availPagSize)
+            <option value="{{$availPagSize}}">{{ucfirst($availPagSize)}}</option>
+        @endforeach
+    </select>
+@endif
 
