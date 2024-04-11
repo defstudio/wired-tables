7a732d4761cd4fdd6b8d1a34a861a0b4891172f9
Fabio Ivona <fabio.ivona@defstudio.it>
2024-04-11 11:23:58 +0200

    fix clamp

--- resources/views/tailwind_3/components/body/content.blade.php
+++ resources/views/tailwind_3/components/body/content.blade.php
@@ -2,10 +2,10 @@
 
 @if($column->get(\DefStudio\WiredTables\Enums\Config::clamp))
     <div class="group relative cursor-default">
-        <div class="line-clamp-1 opacity-100 group-hover:opacity-0 whitespace-normal">
+        <div class="peer line-clamp-1 opacity-100 hover:opacity-0 whitespace-normal">
             {{$slot}}
         </div>
-        <div class="p-2 bg-white opacity-0 group-hover:opacity-100 absolute top-[calc(-0.5rem-1px)] left-[calc(-0.5rem-1px)] right-[calc(-0.5rem-1px)] z-10 box-content border rounded-md whitespace-normal">
+        <div class="z-10 pointer-events-none p-2 bg-white opacity-0 peer-hover:opacity-100 absolute top-[calc(-0.5rem-1px)] left-[calc(-0.5rem-1px)] right-[calc(-0.5rem-1px)] z-9 box-content border rounded-md whitespace-normal">
             {{$slot}}
         </div>
     </div>
